ab1ab49a634cea5099375ad67147f4aafaa87013
NicolineKielberg <83805567+NicolineKielberg@users.noreply.github.com>
2021-11-08 12:32:40 +0100

    navn på class rettet

---
+++ homepage/blivmedlem.js
@@ -0,0 +1,10 @@
+
+document.getElementById("virksomhed").addEventListener("click", function() {
+document.querySelector(".far").style.display = "flex";
+document.querySelector(".mor").style.display = "none";
+})
+
+document.getElementById("privat").addEventListener("click", function() {
+document.querySelector(".far").style.display = "none";
+document.querySelector(".mor").style.display = "flex";
+})
