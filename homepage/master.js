120d5ef63ff14d6e4250e88c2581e2fe8134b5f0
MohamedAbdisamed <Abdisamed@hotmail.dk>
2021-11-08 21:09:12 +0100

    rettet i js for burgermenu
    har gjordt navigationen responsiv, og js koden mere enkel

--- homepage/master.js
+++ homepage/master.js
@@ -5,23 +5,23 @@ document.getElementById("virksomhed").addEventListener("click", function() {
 document.getElementById("privat").addEventListener("click", function() {
 document.querySelector(".far").style.display = "none";
  document.querySelector(".mor").style.display = "flex";
- })
+ });
 //navbar//
 document.addEventListener("DOMContentLoaded", () => {
-   initApp();
-});
-
-function initApp() {
-    alert("pupud");
+    initApp();
+  });
+  
+  const initApp = () => {
     const menuBtn = document.querySelector(".nav__burger-btn");
     const list = document.querySelector(".nav__list");
     menuBtn.addEventListener("click", toggleButtonAndMenu);
     list.addEventListener("click", toggleButtonAndMenu);
-}
-
-const toggleButtonAndMenu = () => {
+    
+  };
+  
+  const toggleButtonAndMenu = () => {
     const menuBtn = document.querySelector(".nav__burger-btn");
     const list = document.querySelector(".nav__list");
     menuBtn.classList.toggle("open");
     list.classList.toggle("open");
-}
+  };
