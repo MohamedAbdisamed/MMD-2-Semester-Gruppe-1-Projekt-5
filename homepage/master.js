b0caeaff69c587c106997e3a4365928dc1a14e69
stefanjacobsen <stja32075@edu.ucl.dk>
2021-11-08 15:11:40 +0100

    Burger menu
    burger nav

--- homepage/master.js
+++ homepage/master.js
@@ -6,3 +6,22 @@ document.getElementById("privat").addEventListener("click", function() {
 document.querySelector(".far").style.display = "none";
  document.querySelector(".mor").style.display = "flex";
  })
+//navbar//
+document.addEventListener("DOMContentLoaded", () => {
+   initApp();
+});
+
+function initApp() {
+    alert("pupud");
+    const menuBtn = document.querySelector(".nav__burger-btn");
+    const list = document.querySelector(".nav__list");
+    menuBtn.addEventListener("click", toggleButtonAndMenu);
+    list.addEventListener("click", toggleButtonAndMenu);
+}
+
+const toggleButtonAndMenu = () => {
+    const menuBtn = document.querySelector(".nav__burger-btn");
+    const list = document.querySelector(".nav__list");
+    menuBtn.classList.toggle("open");
+    list.classList.toggle("open");
+}
